8a051fcbd598268fc70a9622a22da0e59c3427d1
我的二你模仿不来 <94788509+linbuer233@users.noreply.github.com>
2022-07-20 18:26:35 +0800

    Site updated: 2022-07-20 18:26:33

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "1",
+      "tmpColor": "FFFFFF",
+      "tmpSize": "16",
+      "cityColor": "FFFFFF",
+      "citySize": "16",
+      "aqiColor": "FFFFFF",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "0",
+      "language": "auto",
+      "fixed": "true",
+      "vertical": "top",
+      "horizontal": "left",
+      "left": "300",
+      "top": "10",
+      "key": "34e72c7813624fe3b0ea5c00e4e84590"
+    }
+  }
